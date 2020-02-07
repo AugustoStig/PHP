e3f320537ced6f0ef5deb4f26b5dd362c6f8abbd
JOSE AUGUSTO CHAVES IZABEL <06864013103@fnde.gov.br>
2020-02-07 17:51:14 -0300

    Novos arquivos e atualizações de já existentes

---
+++ HelloWorld/Operadores.php
@@ -0,0 +1,28 @@
+<?php
+
+$valor = 100.00;
+
+echo "Valor Inicial = ";
+echo $valor . "<br/>\n";
+
+$valor += 10.00;
+
+echo "100,00 + 10,00 = ";
+echo $valor . "<br/>\n";
+
+$valor -= 10.00;
+
+echo "110.00 - 10.00 = ";
+echo $valor . "<br/>\n";
+
+$valor *= 2;
+
+echo "100,00 * 2 = ";
+echo $valor . "<br/>\n";
+
+$valor /= 2;
+
+echo "200 / 2 = ";
+echo $valor . "<br/>\n";
+
+?>
---
+++ HelloWorld/Operadores3.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<strong>Resultado da multiplicação de 100 * 23 = </strong><br>\n";
+echo 100 * 23;
+echo "<br>\n";
+
+echo "<strong>Resultado da divisão de 100 / 23 = </strong><br>\n";
+echo 100 / 23;
+echo "<br>\n";
+
+echo "<strong>Resultado da sobre da divisão de 100 / 23 = </strong><br>\n";
+echo 100 % 23;
+echo "<br>\n";
+
+echo "<strong>Resultado de 100²³ = </strong><br>\n";
+echo 100 ** 23;
+echo "<br>\n";
+?>
