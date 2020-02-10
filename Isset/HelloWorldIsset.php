05255fc17dcd58c8f69fcc32747fa2bee0d5468c
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-10 17:31:02 -0300

    Atualização do arquivo de Isset

--- Isset/HelloWorldIsset.php
+++ Isset/HelloWorldIsset.php
@@ -1,12 +1,18 @@
 <?php
 
 $Hello = "Hello";
+
 $World = "World";
+
 $Ponto = "!!!";
-$HelloWorld = $Hello." ".$World." ".$Ponto;
+
+$HelloWorld = $Hello." ".$World.$Ponto;
 
 if(isset($HelloWorld)){
+
     echo $HelloWorld."<br/>\n";};
+
+echo "<strong>Passou pelo Isset:</strong><br>\n";
 
 echo "<strong>Existe a variável!!!<strong>";
 ?>
