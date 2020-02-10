8ec346205551169b40141a66233a774819059ea8
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-10 17:20:06 -0300

    Atualização dos Arquivos de Concatenação

--- Concatenacao/HelloWorldConcatenacao.php
+++ Concatenacao/HelloWorldConcatenacao.php
@@ -1,9 +1,18 @@
 <?php
 
 $Hello = "Hello";
+
+echo $Hello."<br>\n";
+
 $World = "World";
+
+echo $World."<br>\n";
+
 $Ponto = "!!!";
-$HelloWorld = $Hello." ".$World." ".$Ponto;
+
+echo $Ponto."<br>\n";
+
+$HelloWorld = "<strong>".$Hello." ".$World." ".$Ponto."</strong>";
 
 echo $HelloWorld;
 
--- Concatenacao/HelloWorldConcatenacao2.php
+++ Concatenacao/HelloWorldConcatenacao2.php
@@ -1,8 +1,16 @@
 <?php
 
 $Hello = "Hello";
+
+echo "<strong>".$Hello."</strong>"."<br>\n";
+
 $World = "World";
+
+echo "<strong>".$World."</strong>"."<br>\n";
+
 $Ponto = "!!!";
+
+echo "<strong>".$Ponto."</strong>"."<br>\n";
 
 echo $Hello." ".$World." ".$Ponto;
 
