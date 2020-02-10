eef6c0aec5b7d300eaf2e821937f197e2c16832d
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-10 17:08:30 -0300

    Atualização Array

--- Array/CarrosArray.php
+++ Array/CarrosArray.php
@@ -2,6 +2,16 @@
 
 $Carros = array("Fiat", "Ford", "Volkswagen", "BMW", "Mercedes-Benz", "Lada", "Dacia", "Renault", "Citroën", "Peugeot");
 
+echo "<strong>Impressão do primeiro número chamado:</strong><br>\n";
+
 echo $Carros[3];
+
+echo "<br>\n";
+
+echo "<strong>Impressão do segundo número chamado:</strong><br>\n";
+
+echo $Carros[7];
+
+echo "<br>\n";
 
 ?>
