1363481d2be4520aae9884d71b1aeb1e60ac6c02
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-11 16:59:57 -0300

    Atualização dos arquivos de Array

--- Array/ArrayCarros.php
+++ Array/ArrayCarros.php
@@ -2,24 +2,20 @@
 
 //Criado por José Augusto Chaves Izabel
 
-// 11/02/2020
+//11/02/2020
 
 //Criação da variável $Carros, que será usada em um array, onde salvará 11 marcas de carro, númerados do número 0 ao 10.
 
 $Carros = array("Fiat", "Ford", "Volkswagen", "BMW", "Mercedes-Benz", "Lada", "Dacia", "Renault", "Citroën", "Peugeot", "Holden");
-
 echo "<strong>Impressão do primeiro número chamado:</strong><br>\n";
 
 //Impressão da marca de número 3, dentro do array.
 
 echo $Carros[3];
-
 echo "<br>\n";
-
 echo "<strong>Impressão do segundo número chamado:</strong><br>\n";
 
 //Impressão da marca de número 7 dentro do array.
 
 echo $Carros[7];
-
 ?>
--- Array/ArrayCoresNomes.php
+++ Array/ArrayCoresNomes.php
@@ -2,7 +2,7 @@
 
 //Criado por José Augusto Chaves Izabel
 
-// 11/02/2020
+//11/02/2020
 
 //Código com a intenção de criar um array, o primeiro com uma lista de cores e o segundo com uma lista de nomes.
 
@@ -11,16 +11,10 @@ $Cores = array("Azul", "Verde", "Vermelho", "Amarelo", "Roxo", "Preto", "Cinza",
 $Nomes = array("Maria", "Edna", "José", "Augusto", "Almir", "Ana", "Luisa", "Roberto", "Ricardo", "Luciana", "Natália", "João");
 
 echo $Cores[7];
-
 echo "<br>\n";
-
 echo $Nomes[3];
-
 echo "<br>\n";
-
 echo $Cores[3];
-
 echo "<br>\n";
-
 echo $Nomes[7];
 ?>
