5783734bcd50a0881e722dc0d6e2b00136500b00
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-11 18:11:24 -0300

    Atualização dos arquivos de Array

--- Array/ArrayFrutas.php
+++ Array/ArrayFrutas.php
@@ -7,6 +7,7 @@
 //Código com a intenção de criar um array de frutas, e após isso imprimir duas delas.
 
 $Frutas = array("Maçã", "Banana", "Jaca", "Melão", "Abacate", "Goiaba", "Limão", "Laranja", "Tangerina", "Manga", "Uva");
+
 echo "<strong>\n".$Frutas[1]."</strong>\n";
 echo "<br>\n";
 echo $Frutas [3];
