9b8829fed13bca3cba587141146cf752088608b3
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-11 15:47:52 -0300

    Atualização dos comentários do arquivo Isset

--- Isset/HelloWorldIsset.php
+++ Isset/HelloWorldIsset.php
@@ -1,5 +1,8 @@
 <?php
 
+/*Código com a intenção de imprimir um "Hello World!!!", porém, utilizado o "Isset", para que a variável só seja impressa caso exista, testando 
+a existência da mesma.*/
+
 $Hello = "Hello";
 
 $World = "World";
@@ -7,6 +10,8 @@ $World = "World";
 $Ponto = "!!!";
 
 $HelloWorld = $Hello." ".$World.$Ponto;
+
+//Uso do "Isset", utilizado um "if" para que se a variável "HelloWorld" existir, imprima o que tem nela.
 
 if(isset($HelloWorld)){
 
