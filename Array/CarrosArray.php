a3d4af3daddb87c9e654bedec293685faa48aa08
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-11 15:31:37 -0300

    Atualização de comentários do Array

--- Array/CarrosArray.php
+++ Array/CarrosArray.php
@@ -1,8 +1,12 @@
 <?php
 
-$Carros = array("Fiat", "Ford", "Volkswagen", "BMW", "Mercedes-Benz", "Lada", "Dacia", "Renault", "Citroën", "Peugeot");
+//Criação da variável $Carros, que será usada em um array, onde salvará 11 marcas de carro, númerados do número 0 ao 10.
+
+$Carros = array("Fiat", "Ford", "Volkswagen", "BMW", "Mercedes-Benz", "Lada", "Dacia", "Renault", "Citroën", "Peugeot", "Holden");
 
 echo "<strong>Impressão do primeiro número chamado:</strong><br>\n";
+
+//Impressão da marca de número 3, dentro do array.
 
 echo $Carros[3];
 
@@ -10,8 +14,8 @@ echo "<br>\n";
 
 echo "<strong>Impressão do segundo número chamado:</strong><br>\n";
 
-echo $Carros[7];
+//Impressão da marca de número 7 dentro do array.
 
-echo "<br>\n";
+echo $Carros[7];
 
 ?>
