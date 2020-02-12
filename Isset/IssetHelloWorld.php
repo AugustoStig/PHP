6caffad900e7d1c0e784876b1f947a8ae49b626d
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-12 14:58:30 -0300

    Atualização do arquivo de Isset

--- Isset/IssetHelloWorld.php
+++ Isset/IssetHelloWorld.php
@@ -1,25 +1,22 @@
 <?php
 
-// 11/02/2020.
+//Criado por José Augusto Chaves Izabel
+
+//11/02/2020
 
 /*Código com a intenção de imprimir um "Hello World!!!", porém, utilizado o "Isset", para que a variável só seja impressa caso exista, testando 
 a existência da mesma.*/
 
 $Hello = "Hello";
-
 $World = "World";
-
 $Ponto = "!!!";
-
 $HelloWorld = $Hello." ".$World.$Ponto;
 
 //Uso do "Isset", utilizado um "if" para que se a variável "HelloWorld" existir, imprima o que tem nela.
 
 if(isset($HelloWorld)){
-
     echo $HelloWorld."<br/>\n";};
 
 echo "<strong>Passou pelo Isset:</strong><br>\n";
-
 echo "<strong>Existe a variável!!!<strong>";
 ?>
