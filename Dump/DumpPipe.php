d4911fe515ca8e3d1192a64e25a25f6838a7a037
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-12 17:57:42 -0300

    Criação de novos arquivos de Dump

---
+++ Dump/DumpPipe.php
@@ -0,0 +1,18 @@
+<?php
+
+//Criado por José Augusto Chaves Izabel
+
+//12/02/2020
+
+//Código criado para testar o uso do "||".
+
+$Calculo = 2 * 3 > 5 || 7 + 2 == 9;
+
+var_dump($Calculo);
+
+echo "<br>\n";
+
+$Calculo2 = 2 * 3 > 5 || 7 + 2 == 10;
+
+var_dump($Calculo2);
+?>
