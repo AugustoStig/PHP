39bfa50e2a734d8de84b1d1199efa741e6916533
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-12 17:44:24 -0300

    Criação de arquivos de Operadores

---
+++ Operadores/OperadorMaisMais.php
@@ -0,0 +1,15 @@
+<?php
+
+//Criado por José Augusto Chaves Izabel
+
+//12/02/2020
+
+//Código criado com a intenção de testar o "++" como operador de incremento de uma variável.
+
+$Valor1 = 0;
+echo $Valor1++."<br>\n";
+echo ++$Valor1."<br>\n";
+echo ++$Valor1."<br>\n";
+echo $Valor1++."<br>\n";
+echo $Valor1;
+?>
---
+++ Operadores/OperadorMenosMenos.php
@@ -0,0 +1,16 @@
+<?php
+
+//Criado por José Augusto Chaves Izabel
+
+//12/02/2020
+
+//Código criado com a intenção de testar o "--" como operador de decremento de uma variável.
+
+$Valor1 = 10;
+
+echo $Valor1--."<br>\n";
+echo --$Valor1."<br>\n";
+echo --$Valor1."<br>\n";
+echo $Valor1--."<br>\n";
+echo $Valor1;
+?>
