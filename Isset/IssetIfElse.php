443dfc1630455ca7d2bad1a24db5ca5073ec8eb1
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-12 15:31:45 -0300

    Criação de arquivo de Isset

---
+++ Isset/IssetIfElse.php
@@ -0,0 +1,36 @@
+<?php
+
+//Criado por José Augusto Chaves Izabel
+
+//12/02/2020
+
+/*Código com a intenção de imprimir um "Hello World!!!", porém, utilizado o "Isset", para que a variável só seja impressa caso exista, testando 
+a existência da mesma.*/
+
+$Nome = "José Augusto Chaves Izabel";
+$Nascimento = 1999;
+
+//Uso do "Isset", utilizado um "if" para que se a variável "HelloWorld" existir, imprima o que tem nela.
+
+if(isset($Nome)){
+    echo $Nome."<br/>\n";
+    echo "<strong>Passou pelo Isset!!</strong><br>\n";
+    echo '<strong>A variável $Nome existe!!</strong>';
+    echo "<br>\n";
+    echo $Nascimento;
+    echo "<br>\n";
+}
+    else{
+        echo "<strong>Não passou pelo Isset!!</strong><br>\n";
+        echo '<strong>A variável $Nome não existe!!</strong>';
+};
+
+if(isset($Teste)){
+    echo $Teste."<br/>\n";
+    echo "<strong>Passou pelo Isset!!</strong><br>\n";
+    echo '<strong>A variável $Teste existe!!</strong>';}
+    else{
+        echo "<strong>Não passou pelo Isset!!</strong><br>\n";
+        echo '<strong>A variável $Teste não existe!!</strong>';
+};
+?>
