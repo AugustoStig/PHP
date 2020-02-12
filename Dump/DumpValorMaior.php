bc5c154834d8085f443a438eadc75cc295a5a15d
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-12 16:31:55 -0300

    Criação de arquivos de Dump

---
+++ Dump/DumpValorMaior.php
@@ -0,0 +1,17 @@
+<?php
+
+//Criado por José Augusto Chaves Izabel
+
+//12/02/2020
+
+//Código criado com a intenção de testar a resposta do "var_dump" "True" ou "False".
+
+$Valor1 = 10;
+$Valor2 = 20;
+
+echo "Valor 1 é igual á $Valor1<br>\n";
+echo "Valor 2 é igual á $Valor2<br>\n";
+echo "$Valor1 é maior que $Valor2?<br>\n";
+
+var_dump($Valor1>$Valor2);
+?>
