c51cc21c815d89bfcc3bfac94339bb11e6c85a5a
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-12 17:58:36 -0300

    Criação de arquivo de cálculo entre parentesis

---
+++ Outros/CalculoParentesis.php
@@ -0,0 +1,15 @@
+<?php
+
+//Criado por José Augusto Chaves Izabel
+
+//12/02/2020
+
+//Código criado para testar o cálculo entre parentesis, para saber qual resultado o PHP daria.
+
+$Calculo = 2 + 2 * 3;
+$Calculo2 = (2 + 2) * 3;
+
+echo $Calculo;
+echo "<br>\n";
+echo $Calculo2;
+?>
