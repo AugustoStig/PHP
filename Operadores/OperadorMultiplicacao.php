4b5a1f2304f1d233225b733f035a7472a5e0e049
José Augusto Chaves Izabel <joseaugusto.gu@gmail.com>
2020-02-12 15:00:35 -0300

    Atualização dos arquivos de Operação

--- Operadores/OperadorMultiplicacao.php
+++ Operadores/OperadorMultiplicacao.php
@@ -1,25 +1,21 @@
 <?php
 
-// 11/02/2020.
+//Criado por José Augusto Chaves Izabel
 
-//Código com a intenção de imprimir o resultado do uso de um operador, o "+=".
+//12/02/2020
+
+//Código com a intenção de imprimir o resultado do uso de um operador, o "*=".
 
 $n1 = 2;
-
 $n2 = 3;
 
 echo "<strong>O número 1 é igual á:</strong><br>\n";
-
 echo $n1."<br>\n";
-
 echo "<strong>O número 2 é igual á:</strong><br>\n";
-
 echo $n2."<br>\n";
 
-$n2 += 2;
+$n2 *= 2;
 
 echo "<strong>O novo número 2 é igual á:</strong><br>\n";
-
 echo $n2;
-
 ?>
--- Operadores/OperadorPontoIgual.php
+++ Operadores/OperadorPontoIgual.php
@@ -1,15 +1,14 @@
 <?php
 
-// 11/02/2020.
+//Criado por José Augusto Chaves Izabel
+
+//11/02/2020
 
 //Código com a intenção de imprimir um "Hello World!!!", utlizando o ".=" para fazer a alteração na variável "Hello".
 
 $Hello = "Hello";
-
 $Ponto = "!!!";
-
 $Hello .= " World";
 
 echo "<strong>".$Hello.$Ponto."</strong>";
-
 ?>
